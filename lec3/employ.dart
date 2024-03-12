1f0dee46b4b6cbc3b99dcf3e267fcc80b386894d
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-03-12 13:21:52 +0530

    Add files via upload

---
+++ lec3/employ.dart
@@ -0,0 +1,65 @@
+
+// Writ a Dart Program to gt an print total three
+// mplo!s information using usr input an
+// Map datatype(us MapEntre class)
+
+// Tak low mntion attriuts for on
+// mplo!
+// id
+// name
+// age
+// salary
+
+import 'dart:io';
+class employee
+{
+    int? id;
+    String? name;
+    int?age;
+    int?salary;
+  
+}
+void main()
+{
+    int id;
+
+    employee e1 = employee();
+
+
+
+  for(int i=0; i<3; i++)
+  {
+  
+   print("Enter ${i+1} employee detail :");
+   stdout.write("Enter name :");
+   String name =stdin.readLineSync()!;
+   e1.name = name;
+
+    stdout.write("Enter id :");
+    e1.id = int.parse(stdin.readLineSync()!);
+
+    stdout.write("Enter age :");
+    e1.age = int.parse(stdin.readLineSync()!);
+
+    stdout.write("Enter salary :");
+    e1.salary = int.parse(stdin.readLineSync()!);
+  
+    Map<String,dynamic> m1 = <String,dynamic>{
+        'id' : e1.id,
+        'name': e1.name,
+        'age' : e1.age,
+        'salary' : e1.salary,
+
+    };
+    
+     print("${i+1} employee detail :- \n\n");
+    
+     print(e1.age);
+     print(e1.id);
+     print(e1.name);
+     print(e1.salary);
+
+   print("\n\n");
+  }
+
+}
