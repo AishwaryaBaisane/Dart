f5084738b6ce99887018a4642badb2d0ccffc1bf
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-03-19 17:25:54 +0530

    Add files via upload

---
+++ pr1/4.dart
@@ -0,0 +1,40 @@
+
+//4.Wap to addition of two matrices di 3x3.
+
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter n:");
+  int n = int.parse(stdin.readLineSync()!);
+  List l1 = [];
+  List l2 = [];
+  List sum = [];
+  for (int i = 0; i < n; i++) {
+    l1.add([]);
+    for (int j = 0; j < n; j++) {
+      stdout.write("Enter ${[i]}");
+      int a = int.parse(stdin.readLineSync()!);
+      l1[i].add(a);
+    }
+  }
+  print("....second array....");
+  for (int i = 0; i < n; i++) {
+    l2.add([]);
+    for (int j = 0; j < n; j++) {
+      stdout.write("Enter ${[i]}");
+      int a = int.parse(stdin.readLineSync()!);
+      l2[i].add(a);
+    }
+  }
+  // sum.add([]);
+  for (int i = 0; i < n; i++) {
+    sum.add([]);
+    for (int j = 0; j < n; j++) {
+      sum[i].add(l1[i][j] + l2[i][j]);
+    }
+  }
+  print(l1);
+  print(l2);
+  stdout.write("sum of elements :");
+  print(sum);
+}
