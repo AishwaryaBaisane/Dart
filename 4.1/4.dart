bacff629018625636da0e214d0803768035068c5
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-03-27 09:40:11 +0530

    Add files via upload

---
+++ 4.1/4.dart
@@ -0,0 +1,32 @@
+// 4. Write a Dart Program to create a function which accepts marks of 3 to 5 occupations and returns
+// a percentage of success to gain a new job as a software developer (using optional parameters).
+
+import 'dart:io';
+
+double per(double flutter,double c,double cplus,double coreflutter)
+{
+    double ans  =  flutter + c + cplus + coreflutter; 
+    ans = ans * 100;
+    return ans/400;
+}
+void main()
+{
+  double flutter,c ,cplus,coreflutter;
+
+  stdout.write("Enter Marks of flutter :");
+  flutter = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter Marks of c :");
+  c = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter Marks of cplus :");
+  cplus = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter Marks of coreflutter :");
+  coreflutter = double.parse(stdin.readLineSync()!);
+
+  
+  double result =  per(flutter,c,cplus,coreflutter);
+  print(result);
+
+}
