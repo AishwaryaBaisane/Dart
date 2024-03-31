3b0633caaf59e6b7c13604ff16c5f75607ff0578
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-03-31 23:32:51 +0530

    Update sudent.dart

--- factory/sudent.dart
+++ factory/sudent.dart
@@ -35,26 +35,26 @@ List student2List = [
        {'name' : 'aaryan','roll_number' : 8,'course' : 'flutter'},
        {'name' : 'aaryan','roll_number' : 9,'course' : 'flutter'},
        {'name' : 'aaryan','roll_number' : 10,'course' : 'flutter'},
-        {'name' : 'aaryan','roll_number' : 1,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 2,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 3,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 4,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 5,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 6,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 7,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 8,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 9,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 10,'course' : 'flutter'}, 
-       {'name' : 'aaryan','roll_number' : 1,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 2,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 3,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 4,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 5,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 6,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 7,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 8,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 9,'course' : 'flutter'},
-       {'name' : 'aaryan','roll_number' : 10,'course' : 'flutter'},
+        {'name' : 'aaryan','roll_number' : 11,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 12,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 13,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 14,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 15,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 16,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 17,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 18,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 19,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 20,'course' : 'flutter'}, 
+       {'name' : 'aaryan','roll_number' : 21,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 22,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 23,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 24,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 25,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 26,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 27,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 28,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 29,'course' : 'flutter'},
+       {'name' : 'aaryan','roll_number' : 30,'course' : 'flutter'},
 ];
 List<studentModel>l1=[];
 
@@ -67,6 +67,6 @@ void main()
   }
   for(int i=0; i<l1.length;i++)
   {
-    stdout.write('name : ${l1[i].name}'+',roll_number : ${l1[i]}');
+    stdout.write('name : ${l1[i].name}'+' ,roll_number : ${l1[i].roll_number}'+' ,course : ${l1[i].course}''\n');
   }
 }
