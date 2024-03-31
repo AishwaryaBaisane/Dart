3328a0b1bfdd28280a9ef60cbe1a3f7d5d80a580
Aishwarya <149373597+AishwaryaBaisane@users.noreply.github.com>
2024-03-31 23:22:42 +0530

    Update factory.dart

--- factory/factory.dart
+++ factory/factory.dart
@@ -1,17 +1,6 @@
 
 import 'dart:io';
-// class personModel{
-//   String? name;
-//   int ? age;
 
-//   personModel({required this.name,required this.age});
-
-//   factory personModel.fromPerson({required Map Person})
-//   {
-//     return personModel(name: Person['name'],age: Person['age']);
-//   }
-
-// }
 class studentModel
 {
   String? name,course;
